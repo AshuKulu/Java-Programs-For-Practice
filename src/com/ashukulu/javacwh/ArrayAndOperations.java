be5a9d32e63701f81ca288c856a2b835f6a353d1
AshuKulu <sahooashutosh247@gmail.com>
2022-01-31 23:41:39 +0530

    1

--- src/com/ashukulu/javacwh/ArrayAndOperations.java
+++ src/com/ashukulu/javacwh/ArrayAndOperations.java
@@ -49,18 +49,69 @@ public class ArrayAndOperations {
 //        float avg=(float)sum/stu;
 //        System.out.println("\nAverage = "+avg);
 
-//        //reverse an array
-//        int [] nums = {64, 71,93,48,32};
+//        //printing array in reverse order
+//        int [] nums = {64,71,93,48,32};
 //        for(int i=nums.length-1; i>=0; i--){
 //            System.out.println(nums[i]);
 //        }
 
-        //maximum and minimum element of array
-        int [] nums = {64, 71,93,48,32};
-        int max = 0;
-        int min = 0;
-        for(int i=0; i<nums.length; i++){
+//        //maximum and minimum element of array
+//        int [] nums = {64, 71,93,-48,32};
+//        int max = Integer.MIN_VALUE;
+//        int min = Integer.MAX_VALUE;
+//        for(int i=0; i<nums.length; i++){
+//            if (nums[i]>max){
+//                max=nums[i];
+//            }
+//        }
+//        System.out.println("Max is "+max);
+//        for(int i=0; i<nums.length; i++){
+//            if (nums[i]<min){
+//                min=nums[i];
+//            }
+//        }
+//        System.out.println("Min is "+min);
 
+//        //2D array addition
+//        int arr1[][]={{1,2,3},{4,5,6}};
+//        int arr2[][]={{9,8,7},{6,5,4}};
+//        for(int i=0; i<arr1.length; i++){
+//            for(int j=0; j<arr1[i].length; j++){
+//                arr1[i][j]+=arr2[i][j];
+//                System.out.print(arr1[i][j]+" ");
+//            }
+//            System.out.println();
+//        }
+
+//        //reversing an array
+//        int [] nums = {64, 71,93,48,32,1,2};
+//        int l= nums.length;;
+//        int n=Math.floorDiv(l,2);
+//        for(int i=0; i<n; i++){
+//            //swap a[i] and a[l-1-i]
+//            int temp = nums[i];
+//            nums[i]=nums[l-1-i];
+//            nums[l-1-i]=temp;
+//        }
+//        for(int item : nums){
+//            System.out.print(item+" ");
+//        }
+
+        //sort an array
+        int [] nums = {64, 71,93,48,32,1,2};
+        for(int i=0; i<nums.length; i++){
+            for (int j=0; j<nums.length; j++){
+                if (nums[i] < nums[j]) {
+                    int temp=nums[i];
+                    nums[i]=nums[j];
+                    nums[j]=temp;
+                }
+            }
         }
+        for(int item : nums){
+            System.out.print(item+" ");
+        }
+
+
     }
 }
