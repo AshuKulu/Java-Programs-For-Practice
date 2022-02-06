7ff2a893539107257514c00b6081209d6d3419ec
AshuKulu <sahooashutosh247@gmail.com>
2022-02-06 22:55:07 +0530

    5

--- src/com/ashukulu/javacwh/Pattern3.java
+++ src/com/ashukulu/javacwh/Pattern3.java
@@ -1,4 +1,7 @@
 package com.ashukulu.javacwh;
 
 public class Pattern3 {
+    public static void main(String[] args) {
+
+    }
 }
--- src/com/ashukulu/javacwh/Pattern4.java
+++ src/com/ashukulu/javacwh/Pattern4.java
@@ -1,4 +1,7 @@
 package com.ashukulu.javacwh;
 
 public class Pattern4 {
+    public static void main(String[] args) {
+
+    }
 }
