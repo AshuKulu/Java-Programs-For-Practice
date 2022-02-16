4448f36c84144d238fd4e096e5f6f447f75f12ad
AshuKulu <sahooashutosh247@gmail.com>
2022-02-16 23:29:24 +0530

    8

---
+++ src/com/ashukulu/javaprograms/Pallindrome.java
@@ -0,0 +1,37 @@
+package com.ashukulu.javaprograms;
+
+import java.util.Scanner;
+
+public class Pallindrome {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        //for integer input
+        int ip = sc.nextInt();
+        int x=ip;
+        int y=0;
+        while(x>0){
+            int z=x%10;
+            y=y*10+z;
+            x=x/10;
+        }
+        if (ip==y){
+            System.out.println("Pallindrome Found");
+        } else {
+            System.out.println("Not a Pallindrome");
+        }
+
+        //for string input
+        String input= sc.next();
+        String reverse;
+        for(int i=(input.length())-1; i>=0; i--){
+            reverse = reverse+input.charAt(i);
+        }
+        System.out.println(input);
+        System.out.println(reverse);
+        if(reverse.equals(input)){
+            System.out.println("Pallindrome Found");
+        } else {
+            System.out.println("Not a Pallindrome");
+        }
+    }
+}
